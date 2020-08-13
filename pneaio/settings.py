9c8cb32683757da6ae0b2c3838d0d0bf8dbd822e
JackSnowdon <jacksnowdondrums@gmail.com>
2020-08-13 14:49:34 +0100

    added and wired up accounts app

--- pneaio/settings.py
+++ pneaio/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'accounts',
     'home',
 ]
 
@@ -127,3 +129,5 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
