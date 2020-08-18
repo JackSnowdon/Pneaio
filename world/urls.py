f4db31b7efb8bfcf62af72349793d3d971d044d8
JackSnowdon <jacksnowdondrums@gmail.com>
2020-08-18 17:17:12 +0100

    added crud functions for cards

--- world/urls.py
+++ world/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('world_index/', world_index, name="world_index"),
     path('create_card/', create_card, name="create_card"),
+    path(r'edit_card/<int:pk>', edit_card, name="edit_card"),
+    path(r'delete_card/<int:pk>', delete_card, name="delete_card"),
 ]
--- world/views.py
+++ world/views.py
@@ -28,6 +28,43 @@ def create_card(request):
     return render(request, "create_card.html", {"card_form": card_form})
 
 
+@login_required
+def edit_card(request, pk):
+    this_card = get_object_or_404(Card, pk=pk)
+    profile = request.user.profile
+    if profile == this_card.created_by or profile.staff_access:
+        if request.method == "POST":
+            card_form = NewCardForm(request.POST, instance=this_card)
+            if card_form.is_valid():
+                form = card_form.save(commit=False)
+                if card_limiter(request, form):
+                    form.save()
+                    messages.error(request, "Edited {0}".format(form.name), extra_tags="alert")
+                    return redirect("world_index")      
+        else:
+            card_form = NewCardForm(instance=this_card)
+        return render(request, "edit_card.html", {"card_form": card_form, "this_card": this_card})
+    else:
+        messages.error(
+            request, "You Don't Have The Required Permissions", extra_tags="alert"
+        )
+        return redirect("world_index")
+
+
+@login_required
+def delete_card(request, pk):
+    this_card = get_object_or_404(Card, pk=pk)
+    if this_card.created_by == request.user.profile:
+        this_card.delete()
+        messages.error(
+            request, f"Deleted {this_card}", extra_tags="alert"
+        )
+        return redirect(reverse("world_index"))
+    else:
+        messages.error(request, f"Card Not Yours To Delete", extra_tags="alert")
+        return redirect("world_index")
+
+
 # Card Helper Functions
 
 def card_limiter(r, card):
