3f163960bb346592ccbd53b88a96b390e34774e8
JackSnowdon <jacksnowdondrums@gmail.com>
2020-08-18 16:11:45 +0100

    created card-limiter function

--- world/models.py
+++ world/models.py
@@ -6,7 +6,7 @@ from accounts.models import Profile
 
 class Card(models.Model):
     name = models.CharField(max_length=255)
-    level = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)], default=0)
+    level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)], default=0)
     attack = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10000)], default=0)
     defense = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10000)], default=0)
     speed = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10000)], default=0)
--- world/views.py
+++ world/views.py
@@ -8,8 +8,7 @@ from .forms import *
 
 @login_required
 def world_index(request):
-    cards = Card.objects.all()
-    print(cards)
+    cards = Card.objects.order_by('-id')[:5]
     return render(request, "world_index.html", {"cards": cards})
 
 
@@ -20,9 +19,22 @@ def create_card(request):
         if card_form.is_valid():
             form = card_form.save(commit=False)
             form.created_by = request.user.profile
-            form.save()
-            messages.error(request, "Started {0}".format(form.name), extra_tags="alert")
-            return redirect("world_index")
+            if card_limiter(request, form):
+                form.save()
+                messages.error(request, "Created {0}".format(form.name), extra_tags="alert")
+                return redirect("world_index")    
     else:
         card_form = NewCardForm()
     return render(request, "create_card.html", {"card_form": card_form})
+
+
+# Card Helper Functions
+
+def card_limiter(r, card):
+    point_limit = card.level * 1750
+    point_total = card.attack + card.defense + card.speed
+    if point_total <= point_limit:
+        return True
+    else:
+        messages.error(r, f"Point Total ({point_total}) Exceeds Limit ({point_limit})", extra_tags="alert")
+        return False
